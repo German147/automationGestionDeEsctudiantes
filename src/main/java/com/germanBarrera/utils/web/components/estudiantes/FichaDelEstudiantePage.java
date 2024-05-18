cfd18c8d2073118f98d0a669c0b80f1d741fd552
Germán <barreragerman27@gmail.com>
2024-05-18 20:32:32 -0300

    It was created FichaDelEstudiantePage tests were created and its related classes.

--- src/main/java/com/germanBarrera/utils/web/components/estudiantes/FichaDelEstudiantePage.java
+++ src/main/java/com/germanBarrera/utils/web/components/estudiantes/FichaDelEstudiantePage.java
@@ -2,18 +2,131 @@ package com.germanBarrera.utils.web.components.estudiantes;
 
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FichaDelEstudiantePage extends AbstractPage {
     @FindBy(css = "#MainContent_lblTitulo")
     private ExtendedWebElement tituloFicha;
 
+    @FindBy(css = "#MainContent_lnkBreveDescripcion")
+    private ExtendedWebElement breveDescriptionBtn;
+
+    @FindBy(css = ".modal-content .headPrimary")
+    private ExtendedWebElement modalTitle;
+
+    @FindBy(css = ".close")
+    private ExtendedWebElement cerrarBtn;
+
+    @FindBy(css = ".col-lg-2 .form-control ")
+    private ExtendedWebElement cicloDropdown;
+
+    @FindBy(css = "#MainContent_ddlGradoAnio")
+    private ExtendedWebElement cursoDropdown;
+
+    @FindBy(css = "#MainContent_txtDni")
+    private ExtendedWebElement dni;
+
+    @FindBy(css = "#MainContent_txtDni")
+    private ExtendedWebElement dniText;
+
+    @FindBy(css = "#MainContent_rptListadoEstudiantes_lblApellido_0")
+    private ExtendedWebElement surname;
+
+    @FindBy(css = ".main-footer")
+    private ExtendedWebElement footer;
+
+    @FindBy(css = "#MainContent_lnkFiltrar")
+    private ExtendedWebElement filtrarBtn;
+
     public FichaDelEstudiantePage(WebDriver driver) {
         super(driver);
     }
 
-    public  boolean isFichaPageOpened(){
+    public boolean isFichaPageOpened() {
         return tituloFicha.isElementPresent();
+    }
+
+    public void openDescription() {
+        breveDescriptionBtn.click();
+    }
+
+    public String descriptionGetTest() {
+        return modalTitle.getText();
+    }
+
+    public void closeModal() {
+        cerrarBtn.click();
+    }
+
+    public void cicloLectivoOptionMenu() {
+        cicloDropdown.click();
+    }
+
+    public void selectOptionFromCicloLectivo(String option) {
+        findDropDownCicloElement().selectByVisibleText(option);
+    }
+
+    private Select findDropDownCicloElement() {
+        return new Select(getDriver().findElement(cicloDropdown.getBy()));
+    }
+
+    public String getSelectedOption() {
+        WebElement element = findDropDownCicloElement().getFirstSelectedOption();
+        String selectedElement = element.getText();
+        return selectedElement;
+    }
+
+    public void clickOnCurso() {
+        WebElement element = getDriver().findElement(cursoDropdown.getBy());
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+    }
+
+    private Select findCurso() {
+        return new Select(getDriver().findElement(cursoDropdown.getBy()));
+    }
+
+    public void selectCurso(int index) {
+        findCurso().selectByIndex(index);
+    }
+
+    public String getSelectedCursoText() {
+        WebElement selectedElement = findCurso().getFirstSelectedOption();
+        return selectedElement.getText();
+    }
+
+    public void dniFillin(String numberDNI) {
+        dni.type(numberDNI);
+    }
+
+    public String getDnitext() {
+        return dniText.getText();
+    }
+
+    public String getStudentSurname() {
+        return surname.getText();
+    }
+
+    public void scrollDownFichaEstudiante() {
+        String scripts = "scroll(0,400);";
+        ((JavascriptExecutor) getDriver()).executeScript(scripts);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(d -> footer.isElementPresent());
+    }
+
+    public void clickOnFiltrar() {
+        filtrarBtn.click();
     }
 }
---
+++ src/test/java/com/germanBarrera/carina/demo/FichaDelEstudianteTests.java
@@ -0,0 +1,98 @@
+package com.germanBarrera.carina.demo;
+
+import com.germanBarrera.carina.demo.base.FichaDelEstudianteBaseTest;
+import com.germanBarrera.carina.demo.base.LoginTest;
+import com.germanBarrera.utils.common.HomePageBase;
+import com.germanBarrera.utils.web.components.SideBarMenu;
+import com.germanBarrera.utils.web.components.estudiantes.EstudiantesDropdownOptions;
+import com.germanBarrera.utils.web.components.estudiantes.FichaDelEstudiantePage;
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+public class FichaDelEstudianteTests extends FichaDelEstudianteBaseTest implements IAbstractTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    SoftAssert softAssert = new SoftAssert();
+
+    @Test(priority = 1)
+    @MethodOwner(owner = "barreraGerman")
+    @TestLabel(name = "estudiantesOptionTest", value = {"web", "functionalTesting"})
+    public void getfichaDelEstudiantePage() {
+        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
+        SideBarMenu sideBar = homePage.getSidebarMenu();
+        EstudiantesDropdownOptions options = sideBar.clickEstudiantesDropdownBtn();
+        options.scrollDownHomePage();
+        FichaDelEstudiantePage ficha = options.fichaEstudianteClick();
+        Assert.assertTrue(ficha.isFichaPageOpened(), "The page was not opened");
+    }
+
+    @Test(priority = 2)
+    @MethodOwner(owner = "barreraGerman")
+    @TestLabel(name = "breveDescriptionTest", value = {"web", "functionalTesting"})
+    public void breveDescriptionTest() {
+        FichaDelEstudiantePage fichaDelEstudiantePage = new FichaDelEstudiantePage(getDriver());
+        fichaDelEstudiantePage.openDescription();
+        String modalTitle = fichaDelEstudiantePage.descriptionGetTest();
+
+        softAssert.assertTrue(fichaDelEstudiantePage.descriptionGetTest().contains(modalTitle), "The modal panel was not opened");
+        fichaDelEstudiantePage.closeModal();
+    }
+
+    @Test(priority = 3)
+    @MethodOwner(owner = "barreraGerman")
+    @TestLabel(name = "cicloLectivoDropdownTest", value = {"web", "functionalTesting"})
+    public void cicloLectivoDropdownTest() {
+        FichaDelEstudiantePage fichaDelEstudiantePage = new FichaDelEstudiantePage(getDriver());
+        fichaDelEstudiantePage.selectOptionFromCicloLectivo("2022");
+        String years = fichaDelEstudiantePage.getSelectedOption();
+        LOGGER.info("The year selected is " + years);
+        softAssert.assertTrue(fichaDelEstudiantePage.getSelectedOption().contains("2012"), "Incorrect option selected in anio");
+    }
+
+    @Test(priority = 4)
+    @MethodOwner(owner = "barreraGerman")
+    @TestLabel(name = "selectCursoTest", value = {"web", "functionalTesting"})
+    public void selectCursoTest() {
+        FichaDelEstudiantePage fichaDelEstudiantePage = new FichaDelEstudiantePage(getDriver());
+        fichaDelEstudiantePage.scrollDownFichaEstudiante();
+        fichaDelEstudiantePage.clickOnCurso();
+        fichaDelEstudiantePage.selectCurso(3);
+        String selectedCurso = fichaDelEstudiantePage.getSelectedCursoText();
+        LOGGER.info("This is the inner text from index 3: " + selectedCurso);
+        softAssert.assertTrue(fichaDelEstudiantePage.getSelectedCursoText().contains("SEGUNDO AÑO \"A\" tarde (687197747)"), "Incorrect option selected in cursos");
+    }
+
+    @Test(priority = 5)
+    @MethodOwner(owner = "barreraGerman")
+    @TestLabel(name = "numDocTest", value = {"web", "functionalTesting"})
+    public void numDocTest() {
+        FichaDelEstudiantePage fichaDelEstudiantePage = new FichaDelEstudiantePage(getDriver());
+        fichaDelEstudiantePage.dniFillin("25068320");
+        String dniNumber = fichaDelEstudiantePage.getDnitext();
+
+        softAssert.assertTrue(fichaDelEstudiantePage.getDnitext().contains(dniNumber), "The DNI is incorrect");
+    }
+
+    @Test(priority = 6)
+    @MethodOwner(owner = "barreraGerman")
+    @TestLabel(name = "searchStudentTest", value = {"web", "functionalTesting"})
+    public void searchStudentTest() {
+        FichaDelEstudiantePage fichaDelEstudiantePage = new FichaDelEstudiantePage(getDriver());
+        fichaDelEstudiantePage.clickOnFiltrar();
+        String surname = fichaDelEstudiantePage.getStudentSurname();
+
+        softAssert.assertTrue(fichaDelEstudiantePage.getStudentSurname().contains(surname), "Selected Student is incorrect");
+    }
+
+
+}
